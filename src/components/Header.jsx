cc8b67b444b5d92426ee86d92ca311f4e7f6d0ef
samirhembrom <samirhembrom007@gmail.com>
2024-10-23 23:20:37 +0530

    implemented search spinner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
   return (
     <>
-      <header className="flex justify-between px-20 py-5 bg-white">
-        <b className="text-xl">Where in the world?</b>
+      <header className="shadow-xl flex justify-between px-20 py-5 bg-white">
+        <Link to="/"><b className="text-xl">Where in the world?</b></Link>
         <button className="font-semibold">Dark Mode</button>
       </header>
     </>
--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData,Link } from "react-router-dom";
+import { useState } from "react";
 
 import { getCountries } from "../../countries"
 
@@ -9,10 +10,11 @@ export async function loader() {
 
 export default function Countries() {
   const countries  = useLoaderData();
+  const [query,setQuery] = useState("");
   // countries.sort((a,b)=>a.country.name.common.localeCompare(b.country.name.common))
 
-  const countriesCard = countries.map(country => (
-    <div key={country.name.common} className="h-80 w-60 bg-white rounded overflow-hidden">
+  const countriesCard = countries.filter(country=>country.name.common.toLowerCase().includes(query)).map(country => (
+    <div key={country.name.common} className="transition duration-500 hover:scale-105 shadow-xl h-80 w-60 bg-white rounded overflow-hidden">
       <Link to={country.name.common}>
         <div className="h-40 size-zero">
           <img className="block h-full w-full object-cover" src={country.flags.svg} alt={country.flag.alt}/>
@@ -30,6 +32,11 @@ export default function Countries() {
 
   return (
     <main className="px-20 py-5">
+      <div className="my-8">
+        <span aria-label="hidden" className="search">
+        <input placeholder='Search for a country' className="shadow-xl h-12 w-96 rounded-sm px-16 py-1" type="text" onChange={(e)=> setQuery(e.target.value)} />
+        </span>
+      </div>
       <div className="flex flex-wrap justify-between gap-8">
         {countriesCard}
       </div>
